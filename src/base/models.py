b962c29df77dd17895bf706aa35018f0310ae662
rommuloifrn <roomulo.cavalkant@outlook.com>
2023-08-27 21:15:43 -0300

    session save working!

--- src/base/models.py
+++ src/base/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
 # Create your models here.
 
 class Session(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=50,blank=True, null=True)
     duration = models.DurationField()
-    when = models.DateTimeField(auto_now=False, auto_now_add=False)
+    when = models.DateTimeField(auto_now=False, null=True)
     notes = models.TextField(max_length=500, null=True, blank=True)
 
     def __str__(self):
--- src/base/views.py
+++ src/base/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Session
+
+from django.utils.dateparse import parse_duration
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+class Chronometer(View):
+    def get(self, request, *args, **kwargs):
+        sessions = Session.objects.all()
+        return render(request, 'index.html', {'sessions':sessions})
+
+    def post(self, request, *args, **kwargs):
+        # request.POST.get('duration')
+        sess = Session(duration=parse_duration(request.POST.get('duration')))
+        sess.save()
+        
+
+        return HttpResponseRedirect('')
