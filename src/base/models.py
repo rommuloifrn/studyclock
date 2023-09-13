8e545aff797df4e545243fc917cc26ffc7a3c6cf
Rômulo da Silva <roomulo.cavalkant@outlook.com>
2023-09-13 13:39:54 +0000

    complete the mannual session insertion implementation

--- src/base/models.py
+++ src/base/models.py
@@ -9,7 +9,7 @@ class Session(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50,blank=True, null=True)
     duration = models.DurationField()
-    saved = models.DateTimeField(auto_now_add=True) # https://docs.djangoproject.com/en/4.1/topics/i18n/timezones/
+    saved = models.DateTimeField() # https://docs.djangoproject.com/en/4.1/topics/i18n/timezones/
     last_edit = models.DateTimeField(auto_now=True)
     notes = models.TextField(max_length=500, null=True, blank=True)
 
