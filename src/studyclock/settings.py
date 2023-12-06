1b5731ca4265fbf906caa2c3717e23cb970f3f4a
rommuloifrn <roomulo.cavalkant@outlook.com>
2023-12-06 20:31:59 -0300

    add SESSION_COOKIE_AGE in settings

--- src/studyclock/settings.py
+++ src/studyclock/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+SESSION_COOKIE_AGE = 631139040 # 20 years
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
